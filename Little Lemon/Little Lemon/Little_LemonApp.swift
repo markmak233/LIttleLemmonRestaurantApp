7111ebb85c899e6b9a67e12f5fd629810ae211b2
markmak233 <90290156+markmak233@users.noreply.github.com>
2024-01-19 23:08:57 -0700

    new files

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by marco mai on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
